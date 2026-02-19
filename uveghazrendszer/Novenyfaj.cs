9a8dbb3b504c05578de5240613030ff2c7407d25
Csidom <csikszi.dome@gmail.com>
2026-02-19 14:51:52 +0100

    növényfaj osztály kész

--- uveghazrendszer/Novenyfaj.cs
+++ uveghazrendszer/Novenyfaj.cs
@@ -8,5 +8,29 @@ namespace uveghazrendszer
 {
 	internal class Novenyfaj
 	{
+		private string nev;
+		private int vizigeny; // max 100, azon felül egészségtelen
+		private int egeszsegiAllapot; // 0-10 --> 0: gatya(halott)
+		private int optimalSuruseg;
+
+		public Novenyfaj(string nev, int vizigeny, int egeszsegiAllapot, int optimalSuruseg)
+		{
+			this.nev = nev;
+			this.vizigeny = vizigeny;
+			this.egeszsegiAllapot = egeszsegiAllapot;
+			this.optimalSuruseg = optimalSuruseg;
+		}
+
+		public string Azonosito { get => this.nev.Substring(0, 3); }
+		public string Nev { get => nev; set => nev = value; }
+		public int Vizigeny { get => vizigeny; set => vizigeny = value; }
+		public int EgeszsegiAllapot { get => egeszsegiAllapot; set => egeszsegiAllapot = value; }
+		public int OptimalSuruseg { get => optimalSuruseg; set => optimalSuruseg = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.Azonosito}) {this.vizigeny} {this.egeszsegiAllapot}";
+		}
+	
 	}
 }
--- uveghazrendszer/Program.cs
+++ uveghazrendszer/Program.cs
@@ -9,8 +9,7 @@
 			Kezelo k = new Kezelo("Kovács Péter", "kpeter", Szerepkor.ADMIN);
             Kezelo k1 = new Kezelo("Kiss Anna", "kanna", Szerepkor.KERTESZ);
 
-			Console.WriteLine(k);
-			Console.WriteLine(k1);
+
 		}
     }
 }
