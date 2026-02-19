8da5472a85c82ada3dd7e3d9b21664986fca0d88
Csidom <csikszi.dome@gmail.com>
2026-02-19 14:41:52 +0100

    kezelő osztály kész

---
+++ uveghazrendszer/Enumok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uveghazrendszer
+{
+	enum Szerepkor
+	{
+		KERTESZ,
+		TECHNIKUS,
+		ADMIN
+	}
+
+	internal class Enumok
+	{
+	}
+}
--- uveghazrendszer/Program.cs
+++ uveghazrendszer/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			Kezelo k = new Kezelo("Kovács Péter", "kpeter", Szerepkor.ADMIN);
+            Kezelo k1 = new Kezelo("Kiss Anna", "kanna", Szerepkor.KERTESZ);
+
+			Console.WriteLine(k);
+			Console.WriteLine(k1);
+		}
     }
 }
