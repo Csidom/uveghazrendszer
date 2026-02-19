a2216ff7232848b962cb56436d09b925c4fc889e
Csidom <csikszi.dome@gmail.com>
2026-02-19 15:09:28 +0100

    cella osztály kész

--- uveghazrendszer/Program.cs
+++ uveghazrendszer/Program.cs
@@ -6,10 +6,15 @@
 
         static void Main(string[] args)
         {
-			Kezelo k = new Kezelo("Kovács Péter", "kpeter", Szerepkor.ADMIN);
+			Kezelo k = new Kezelo("Nagy Antal", "nantal", Szerepkor.ADMIN);
             Kezelo k1 = new Kezelo("Kiss Anna", "kanna", Szerepkor.KERTESZ);
 
+            Novenyfaj n1 = new Novenyfaj("Paradicsom", 100, 10, 10);
+            Cella cella = new Cella(new Pozicio(0, 0));
+            Console.WriteLine(cella.UresCella);
 
+            cella.Beultet(n1, 5);
+			Console.WriteLine(cella.UresCella);
 		}
     }
 }
